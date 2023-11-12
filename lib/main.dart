e09e596c05721e06d2031e8e843ec631909a42bb
Radiocephas <akinnusioluwatobiloba@gmail.com>
2023-11-12 09:32:35 +0100

    New commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: ProfileApp(),
   ));
+}
+
+class ProfileApp extends StatelessWidget {
+  const ProfileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('IMG_9672.JPG'),
+        backgroundColor: Colors.green[900],
+      ),
+      body: Center(
+        child: Image.asset('images/IMG_9672.JPG'),
+      ),
+    );
+  }
 }
