6bf8e1cc7c5b0456d622abf4d99d77334370aaac
Radiocephas <akinnusioluwatobiloba@gmail.com>
2023-11-12 09:32:51 +0100

    Made changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: ProfileApp(),
-  ));
+  runApp(MaterialApp(title: 'Text App', home: TextApp()));
 }
 
-class ProfileApp extends StatelessWidget {
-  const ProfileApp({super.key});
+class TextApp extends StatefulWidget {
+  const TextApp({super.key});
 
+  @override
+  State<TextApp> createState() => _TextAppState();
+}
+
+class _TextAppState extends State<TextApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('IMG_9672.JPG'),
-        backgroundColor: Colors.green[900],
-      ),
-      body: Center(
-        child: Image.asset('images/IMG_9672.JPG'),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+            child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            
+          ),
+        )),
       ),
     );
   }
